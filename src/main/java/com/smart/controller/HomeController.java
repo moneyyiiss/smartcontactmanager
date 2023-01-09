65eaa7a84f10989dc0511bf60a8a9b4b82d5651e
Manish Mani Dinkar <moneyyiiss@gmail.com>
2023-01-09 17:55:28 +0530

    signup page done

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -1,21 +1,30 @@
 package com.smart.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.User;
+import com.smart.helper.Message;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@GetMapping("/test")
 	@ResponseBody
 	public String test() {
@@ -25,22 +34,62 @@ public class HomeController {
 		userRepository.save(user);
 		return "Working properly";
 	}
-	
+
 	@RequestMapping("/")
 	public String home(Model model) {
 		model.addAttribute("title", "Home - Smart Contact Manager");
 		return "home";
 	}
-	
+
 	@RequestMapping("/about")
 	public String about(Model model) {
 		model.addAttribute("title", "About - Smart Contact Manager");
 		return "About";
 	}
-	
+
 	@RequestMapping("/signup")
 	public String signup(Model model) {
 		model.addAttribute("title", "Register - Smart Contact Manager");
+		model.addAttribute("user", new User());
 		return "signup";
+	}
+
+	// handler for registering user
+	@RequestMapping(value="/do_register", method = RequestMethod.POST)
+	public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result1, @RequestParam(value="agreement", defaultValue = "false") boolean agreement, Model model ,HttpSession session) {
+		try {
+			if(!agreement) {
+				System.out.println("You have not agreed the terms and conditions");
+				throw new Exception("You have not agreed the terms and conditions");
+			}
+			
+			if(result1.hasErrors()) {
+				System.out.println("ERROR " + result1.toString());
+				model.addAttribute("user", user);
+				return "signup";
+			}
+			
+			
+			user.setRole("ROLE_USER");
+			user.setEnabled(true);
+			user.setImageUrl("default.png");
+			System.out.println("Agreement " + agreement);
+			System.out.println("USER "+ user);
+			
+			User result = this.userRepository.save(user);
+			
+			model.addAttribute("user", new User());
+			
+			session.setAttribute("message", new Message("Successfully Registered !!" , "alert-success"));
+			return "signup";
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			model.addAttribute("user", user);
+			session.setAttribute("message", new Message("Something Went wrong !! " + e.getMessage(), "alert-danger"));
+			return "signup";
+		}
+		
 	}
 }
--- src/main/java/com/smart/entities/User.java
+++ src/main/java/com/smart/entities/User.java
@@ -3,6 +3,8 @@ package com.smart.entities;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,6 +14,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 
 @Entity
 @Table(name="USER")
@@ -20,6 +25,9 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	
+	@NotBlank(message="Name field is required !!")
+	@Size(min = 2, max=20, message= "min 2 and max 20 characters are allowed !!")
 	private String name;
 	@Column(unique = true)
 	private String email;
@@ -92,6 +100,13 @@ public class User {
 	public void setContacts(List<Contact> contacts) {
 		this.contacts = contacts;
 	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", role=" + role
+				+ ", enabled=" + enabled + ", imageUrl=" + imageUrl + ", about=" + about + ", contacts=" + contacts
+				+ "]";
+	}
+	
 	
 	
 	
